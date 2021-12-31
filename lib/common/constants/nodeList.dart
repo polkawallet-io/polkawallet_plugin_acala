7bb1d31348053acb86d61c187ee6f492c8e0ffca
dingzuhua <dingzuhua@qq.com>
2021-12-31 15:58:00 +0800

    tokens images update

--- lib/common/constants/nodeList.dart
+++ lib/common/constants/nodeList.dart
@@ -2,13 +2,33 @@ import 'package:polkawallet_plugin_acala/common/constants/base.dart';
 
 const node_list = [
   {
-    'name': 'Acala Mandala (Hosted by Acala Network)',
+    'name': 'Acala (via Polkawallet)',
     'ss58': ss58_prefix_acala,
-    'endpoint': 'wss://mandala6.laminar.codes',
+    'endpoint': 'wss://acala.polkawallet.io',
   },
   {
-    'name': 'Mandala TC6 Node 1 (Hosted by OnFinality)',
+    'name': 'Acala (via Acala Foundation 0)',
     'ss58': ss58_prefix_acala,
-    'endpoint': 'wss://node-6775973502614921216.rz.onfinality.io/ws',
+    'endpoint': 'wss://acala-rpc-0.aca-api.network',
+  },
+  {
+    'name': 'Acala (via Acala Foundation 1)',
+    'ss58': ss58_prefix_acala,
+    'endpoint': 'wss://acala-rpc-1.aca-api.network',
+  },
+  {
+    'name': 'Acala (via Acala Foundation 2)',
+    'ss58': ss58_prefix_acala,
+    'endpoint': 'wss://acala-rpc-2.aca-api.network/ws',
+  },
+  {
+    'name': 'Acala (via Acala Foundation 3)',
+    'ss58': ss58_prefix_acala,
+    'endpoint': 'wss://acala-rpc-3.aca-api.network/ws',
+  },
+  {
+    'name': 'Acala (via OnFinality)',
+    'ss58': ss58_prefix_acala,
+    'endpoint': 'wss://acala-polkadot.api.onfinality.io/public-ws',
   },
 ];
